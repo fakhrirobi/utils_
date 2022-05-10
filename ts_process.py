1e751fb79b981dd889c1c7db6837ef7b748764e7
Fakhri Robi Aulia <fakhrirobi.fra@gmail.com>
2022-05-11 05:04:33 +0700

    adding pacf and acf plot

--- ts_process.py
+++ ts_process.py
@@ -5,6 +5,28 @@ from sklearn.utils.validation import _num_samples
 import pandas as pd 
 import numpy as np 
 
+def create_corr_plot(series, plot_pacf=False):
+    import plotly.graph_objects as go 
+    corr_array = pacf(series.dropna(), alpha=0.05) if plot_pacf else acf(series.dropna(), alpha=0.05)
+    lower_y = corr_array[1][:,0] - corr_array[0]
+    upper_y = corr_array[1][:,1] - corr_array[0]
+
+    fig = go.Figure()
+    [fig.add_scatter(x=(x,x), y=(0,corr_array[0][x]), mode='lines',line_color='#3f3f3f') 
+     for x in range(len(corr_array[0]))]
+    fig.add_scatter(x=np.arange(len(corr_array[0])), y=corr_array[0], mode='markers', marker_color='#1f77b4',
+                   marker_size=12)
+    fig.add_scatter(x=np.arange(len(corr_array[0])), y=upper_y, mode='lines', line_color='rgba(255,255,255,0)')
+    fig.add_scatter(x=np.arange(len(corr_array[0])), y=lower_y, mode='lines',fillcolor='rgba(32, 146, 230,0.3)',
+            fill='tonexty', line_color='rgba(255,255,255,0)')
+    fig.update_traces(showlegend=False)
+    fig.update_xaxes(range=[-1,42])
+    fig.update_yaxes(zerolinecolor='#000000')
+    
+    title='Partial Autocorrelation (PACF)' if plot_pacf else 'Autocorrelation (ACF)'
+    fig.update_layout(title=title)
+    fig.show()
+    
 def get_metrics(ytrue,yhat) : 
     from sklearn.metrics import (mean_absolute_error,
                                 mean_absolute_percentage_error,
